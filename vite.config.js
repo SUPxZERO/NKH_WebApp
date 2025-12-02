c7e02272ef7b9ce2444c2b676d212c5f6167af05
SUBxZERO <bangboy892005@gmail.com>
2025-12-02 16:00:45 +0700

    vite commit

--- vite.config.js
+++ vite.config.js
@@ -16,12 +16,5 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'resources/js'),
         },
     },
-    server: {
-        host: '127.0.0.1',
-        port: 5173,
-        strictPort: true,
-        hmr: {
-            host: '127.0.0.1',
-        },
-    },
+
 });
